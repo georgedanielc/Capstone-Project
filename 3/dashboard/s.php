df3a303b84f4ebcf87e937e6dc8881757a8047e2
georgedanielc <123776595+georgedanielc@users.noreply.github.com>
2023-01-31 19:54:06 +0800

    Capstone Project

--- 3/dashboard/s.php
+++
@@ -1,5 +0,0 @@
- <?php  
- $connect = mysqli_connect("localhost", "root", "", "barangaymahabangparang");  
- $query ="SELECT * FROM resident ORDER BY resident_id DESC";  
- $result = mysqli_query($connect, $query);  
- ?>  
--- 3/dashboard/sessiondes.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-session_start();
-
-unset($_SESSION["id"]);
-header('location:../index.php');
-
-?>
